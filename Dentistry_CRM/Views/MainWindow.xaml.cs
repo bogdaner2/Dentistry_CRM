7d954c3d1cca59e4ea756a762b99e23911aa4e3a
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-11-01 22:42:27 +0200

    create basic structure

---
+++ Dentistry_CRM/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Dentistry_CRM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
